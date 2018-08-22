ab7d83d480383c57f2b5ddfcee21c725af5cd285
Edison Zhou <edison_x_zhou@manulife.com>
2018-08-22 10:46:25 +0800

    完善msadv18 : 实现邮件发送 & 使用spring boot actuator监控端点

--- src/msadv18/src/main/java/com/manulife/msadv18/Msadv18Application.java
+++ src/msadv18/src/main/java/com/manulife/msadv18/Msadv18Application.java
@@ -8,11 +8,12 @@ public class Msadv18Application {
 
 	/*
 	 * 该示例主要演示以下内容：
-	 * 使用JavaMailSender实现邮件发送，包括：
-	 * 1.简单文本邮件
-	 * 2.带附件的邮件
-	 * 3.带嵌入资源的邮件
-	 * 4.基于Thymeleaf模板的邮件
+	 * 1 使用JavaMailSender实现邮件发送，包括：
+	 * 	1.1 简单文本邮件
+	 * 	1.2 带附件的邮件
+	 * 	1.3 带嵌入资源的邮件
+	 * 	1.4 基于Thymeleaf模板的邮件
+	 * 2 使用Spring Boot Actuator监控端点
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Msadv18Application.class, args);
